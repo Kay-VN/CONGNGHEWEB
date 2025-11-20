1a51990b0e607ca8ad5ec0aa12d2f4abca5f4be9
Kay-VN <manhbuibmd@gmail.com>
2025-11-20 23:10:40 +0700

    PHT

---
+++ Chapter.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html> 
+<html lang="vi"> 
+<head> 
+<meta charset="UTF-8">
+<title>PHT Chương 2 - PHP Căn Bản</title> 
+</head> 
+
+<body>
+<h1>Kết quả PHP Căn Bản</h1>
+
+<?php
+
+
+$ho_ten = "Bùi Mạnh Đức"; 
+$diem_tb = 10;
+$co_di_hoc_chuyen_can = true;
+
+
+echo "Họ tên: " . $ho_ten . "<br>";
+echo "Điểm: " . $diem_tb . "<br><br>"; 
+
+
+if ($diem_tb >= 8.5 && $co_di_hoc_chuyen_can == true) {\
+    echo "Xếp loại: Giỏi"; 
+} elseif ($diem_tb >= 6.5 && $co_di_hoc_chuyen_can == true) {
+    echo "Xếp loại: Khá";
+} elseif ($diem_tb >= 5.0 && $co_di_hoc_chuyen_can == true) {
+    echo "Xếp loại: Trung bình";
+} else {
+
+    echo "Xếp loại: Yếu (Cần cố gắng thêm!)";
+}
+
+echo "<br><br>";
+
+
+function chaoMung() {
+    echo "Chúc mừng bạn đã hoàn thành PHT Chương 2!"; 
+}
+chaoMung();
+
+
+// KẾT THÚC CODE PHP CỦA BẠN TẠI ĐÂY
+?> 
+ 
+</body> 
+</html>
